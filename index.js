9b5d2e7cb9e5654fd994e5cdd153daab16a11cb4
dino0908 <dinokhaw@yahoo.com.sg>
2023-11-28 20:31:34 +0800

    starting template

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const { v4: uuidv4 } = require('uuid');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
 
-app.get('/chat', (req, res) => {
-    res.sendFile(__dirname + '/chat.html');
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
+
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log(socket.id);
+    const username = 'test';
+    io.emit("chat message", { username, msg });
   });
 
-const registeredUsers = {};
-
-io.on('connection', (socket) => {
-
-    
-
-    socket.on('chat message', (msg) => {
-        const username = registeredUsers[socket.userId];
-        io.emit('chat message', {username, msg});
-      });
-
-    socket.on('set username', (username) => {
-        const userId = uuidv4();
-        registeredUsers[userId] = username;
-        socket.userId = userId;
-    })
+  socket.on("set username", (username) => {
+    console.log(socket.id);
   });
+});
 
 server.listen(8080, () => {
-  console.log('listening on port:8080');
+  console.log("listening on port:8080");
 });
