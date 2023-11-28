e835341b837bbbe39e3ef6e72450689f2c2b644b
dino0908 <dinokhaw@yahoo.com.sg>
2023-11-28 21:19:46 +0800

    Standardize socket id with localstorage

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const usernames = [];
+const accounts = {};
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/login.html");
 });
@@ -15,14 +18,26 @@ app.get("/chat", (req, res) => {
 
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    console.log(socket.id);
-    const username = 'test';
+  socket.on("chat message", (data) => {
+    console.log('here', data);
+    var msg = data.inp;
+    var userId = data.userId;
+    var username = accounts[userId];
     io.emit("chat message", { username, msg });
   });
 
-  socket.on("set username", (username) => {
-    console.log(socket.id);
+  socket.on("set username", (data) => {
+    console.log('data', data);
+    if (!usernames.includes(data.username)) {
+      //handle set username
+      usernames.push(data.username);
+      accounts[data.userId] = data.username;
+
+    } else {
+      console.log('username ' + data.username + ' taken, sorry!');
+    }
+    console.log('usernames: ', usernames);
+    console.log('accounts: ', accounts);
   });
 });
 
