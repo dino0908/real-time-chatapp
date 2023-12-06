23a30d76e528ef74c8eeef528c59cbf85c3a5ca7
dino0908 <dinokhaw@yahoo.com.sg>
2023-12-07 01:21:11 +0800

    fix incorrect urls

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://dinos3bucket.s3-website-ap-southeast-2.amazonaws.com",
+    origin: "http://dinos3bucketdeploy.s3-website-ap-southeast-2.amazonaws.com",
     methods: ["GET", "POST"]
   }
 });
