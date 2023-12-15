ff1ae70035ac0d83894a528406fee102e3bbee68
dino0908 <dinokhaw@yahoo.com.sg>
2023-12-15 23:31:26 +0800

    landing page first iteration

--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./Register";
+import Landing from "./Landing";
 import Chat from "./Chat";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -9,7 +10,7 @@ function App() {
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/chat" element={<Chat />} />
         </Routes>
       </Router>
