07e686f63913ed99acfad46b7592dd46085a5dcc
dino0908 <dinokhaw@yahoo.com.sg>
2024-04-01 20:04:16 +0800

    fix login issue

--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -24,8 +24,6 @@ function App() {
             <Route path="/settings" element={<Settings/>} />
           </Route>
 
-
-          
         </Routes>
       </Router>
     </ChakraProvider>
--- client/react/src/routes/PrivateRoute.jsx
+++ client/react/src/routes/PrivateRoute.jsx
@@ -3,26 +3,27 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { getUserID } from '../firebase';
 
 const PrivateRoute = () => {
-
-    const [currentUser, setCurrentUser] = useState(null);
+    const [authenticated, setAuthenticated] = useState(null);
 
     useEffect(() => {
-        const fetchUserID = async () => {
-        try {
-            const userID = await getUserID();
-            setCurrentUser(userID);
-        } catch (error) {
-            // Handle error if getUserID fails
-            console.error('Error fetching user ID:', error);
-        }
+        const checkAuthentication = async () => {
+            try {
+                const userID = await getUserID();
+                // Set authenticated to true if user is authenticated
+                setAuthenticated(!!userID); // Convert userID to a boolean
+            } catch (error) {
+                // Handle error if getUserID fails
+                console.error('Error fetching user ID:', error);
+                setAuthenticated(false); // Set authenticated to false
+            }
         };
 
-    fetchUserID();
-  }, []);
+        checkAuthentication();
+    }, []);
 
-    // If authorized, return an outlet that will render child elements
-    // If not, return element that will navigate to login page
-    return currentUser ? <Outlet /> : <Navigate to="/login" />;
+    // If authenticated, render child elements
+    // If not authenticated, redirect to the login page
+    return authenticated === null ? null : authenticated ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default PrivateRoute;
